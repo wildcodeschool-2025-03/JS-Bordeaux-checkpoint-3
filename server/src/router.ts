c7297c5ad9de87caa163b50923aeb590e7d79cee
Valentin Ferreira <ferreira.pro37@gmail.com>
2025-06-23 11:36:03 +0200

    finished step 3

--- server/src/router.ts
+++ server/src/router.ts
@@ -12,6 +12,7 @@ import boatActions from "./modules/boat/boatActions";
 
 router.get("/api/boats", boatActions.browse);
 
+router.put("/api/boats/:id", boatActions.edit)
 import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
