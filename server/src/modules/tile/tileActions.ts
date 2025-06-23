9685796aeb42276c08c8364103e5f99f93b3349e
BICHON <imane.bichon@icloud.com>
2025-06-23 13:51:02 +0200

    back : finish step 4 for validate middleware

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,11 +1,34 @@
+import { DatabaseModule } from "@faker-js/faker/.";
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const tiles = await tileRepository.readAll();
+    res.json(tiles);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const coord_x = Number(req.body.coord_x);
+    const coord_y = Number(req.body.coord_y);
+
+    if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+      res.sendStatus(422);
+      return;
+    }
+    next();
+    const valid = await tileRepository.readByCoordinates(coord_x, coord_y);
+    if (!valid) {
+      res.sendStatus(422);
+    }
+    res.json(valid);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
