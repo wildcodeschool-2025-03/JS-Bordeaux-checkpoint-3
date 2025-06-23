083b3c6abc4a0090c4536b6ab421ccf334df3c93
Morelle <cynthia.morelle.cm@gmail.com>
2025-06-23 13:02:46 +0200

    almost finished step4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -13,7 +13,12 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+    const tile = await tileRepository.findByCoordinates(coord_x, coord_y);
+
+    next();
+  } catch (err) {}
 };
 
 export default {
