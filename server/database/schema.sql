c7b74921d741626a4cc7a828caa8157ccfff0b28
Yakup <yakup.senel33@gmail.com>
2025-06-23 09:35:22 +0200

    finished step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -14,6 +14,9 @@ CREATE TABLE tile (
     PRIMARY KEY (id)
 );
 
+ALTER TABLE tile
+ADD has_treasure BOOLEAN NOT NULL DEFAULT false;
+
 INSERT INTO
     boat (name, coord_x, coord_y)
 VALUES ("Black Pearl", 1, 1),
