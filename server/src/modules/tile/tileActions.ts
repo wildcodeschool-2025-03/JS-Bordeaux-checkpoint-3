f477176dffac6f3743f1c270c60bf7026b70cb88
tcrusel <crusel.thomas@gmail.com>
2025-06-23 12:37:14 +0200

    almost finished step 4

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -3,19 +3,31 @@ import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch all boats from the database
     const tiles = await tileRepository.readAll();
 
-    // Respond with the boats in JSON format
     res.json(tiles);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+    const tileCoords = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (
+      !coord_x ||
+      !coord_y ||
+      typeof coord_x !== "number" ||
+      typeof coord_y !== "number"
+    ) {
+      res.sendStatus(422);
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
